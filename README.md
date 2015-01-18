21fc102a400428211f65c056386ccc7f5c724231
Manuel Alabor <msites@msites.net>
2015-01-19 00:15:30 +0100

    initial commit after splitting js and web api

---
+++ README.md
@@ -0,0 +1,3 @@
+# orchestra-webapi
+
+`orchestra-webapi` is an adapter for [orchestra-jsapi](https://github.com/swissmanu/orchestra-jsapi). It leverages web technologies as REST and WebSockets to forward its functionality via HTTP.
---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express')
+	, app = express()
+	, Universe = require('orchestra-jsapi')
+
+	, universe = new Universe()
+	, restApi = require('./src/rest')
+	, webSocketApi = require('./src/websocket');
+
+app.set('universe', universe);
+restApi(app);
+
+// Expose two properties:
+//  - restApi with the express api app
+//  - webSocketApi function that takes an http server, ready to be attached to a primus instance.
+//  - webSocketApiConfig object, used to configure primus. helpful to generate the primus client library.
+module.exports = {
+	restApi: app
+	, webSocketApi: webSocketApi.bind(null, universe)
+	, webSocketApiConfig: require('./src/websocket/config')
+};
